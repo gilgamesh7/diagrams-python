f2da69d4f119dd275ac5314a1b92d86056fa8604
gilgamesh7 <felidaesrule@yahoo.com>
2023-03-26 17:44:38 +1300

    Move pictures to diagrams directory

--- app_aws_example.py
+++ app_aws_example.py
@@ -3,8 +3,13 @@ from diagrams.aws.compute import EC2
 from diagrams.aws.database import RDS
 from diagrams.aws.network import ELB
 
+from pathlib import Path
+diagrams_folder = Path.cwd() / 'diagrams'
+diagrams_folder.mkdir(parents=True, exist_ok=True)
 
+aws_diagram =  diagrams_folder / 'aws_web_service'
+print(aws_diagram)
 
-with Diagram("Web Service", show=False):
+with Diagram(str(aws_diagram), show=False):
     ELB("lb") >> EC2("web") >> RDS("userdb")
 
